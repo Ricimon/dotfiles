1d7845d6043abdced99673a5e4ad2f0493fed364
Ricimon <ricimon25@gmail.com>
2020-03-08 23:40:53 -0700

    add rvm support, add gitignore

--- .common.bash
+++ .common.bash
@@ -11,6 +11,13 @@ fi
 # computer-specific bash configs
 [ -f ~/.config.bash ] && source ~/.config.bash
 
+# Don't let RVM set up shell to load RVM
+export rvm_ignore_dotfiles=yes
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
+# Load RVM into a shell session *as a function*
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" 
+
 # end-of-shell-initialization commands
 # enable pyenv shims and autocompletion
 command -v pyenv 1>/dev/null 2>&1 && eval "$(pyenv init -)"
