eaeb858d6b56555e3a12a491f34ce81b361c247a
Ricimon <ricimon25@gmail.com>
2025-11-20 21:26:47 -0800

    updated nvim rust config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -30,9 +30,41 @@ return {
 		},
 	},
 
+  -- Collection of common configurations for the Nvim LSP client
+  "neovim/nvim-lspconfig",
+  -- Visualize lsp progress
+  {
+    "j-hui/fidget.nvim",
+    config = function()
+      require("fidget").setup()
+    end
+  },
+
+  -- Autocompletion framework
+  "hrsh7th/nvim-cmp",
+  {
+    -- cmp LSP completion
+    "hrsh7th/cmp-nvim-lsp",
+    -- cmp Snippet completion
+    "hrsh7th/cmp-vsnip",
+    -- cmp Path completion
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-buffer",
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+    }
+  },
+  -- Snippet engine
+  "hrsh7th/vim-vsnip",
+  -- Adds extra functionality over rust analyzer
   {
     "mrcjkb/rustaceanvim",
     version = "^6",
     lazy = false,
   },
+
+  -- Optional
+  "nvim-lua/popup.nvim",
+  "nvim-lua/plenary.nvim",
+  "nvim-telescope/telescope.nvim",
 }
